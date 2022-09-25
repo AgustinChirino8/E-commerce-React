1a1e7ca0924ae40d45116d9e9e2781159d3bce56
AgustinChirino8 <agustinchirino8@gmail.com>
2022-09-25 19:50:41 -0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
 import "./App.css";
-import "../src/components/Main/Main.css"
+import "../src/components/Main/Main.css";
 import NavBar from "./components/NavBar/navBar";
 import Title from "./components/Title/Title";
 import ItemListContainer from "./components/itemListContainer/itemListContainer";
 import Main from "./components/Main/Main";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App({ texto }) {
   return (
     <>
-      <NavBar />
-      <div className="App" id="fondo">
-        <Title greeting={texto} />
-        <Main />
-        <div className="bloque">
-          <ItemListContainer texto="Cuurly's Coffee Shop" />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div className="bloque">
+                <ItemListContainer texto="Cuurly's Coffee Shop" />
+              </div>
+            }
+          />
+          <Route
+            path="/categoria"
+            element={
+              <div className="bloque">
+                <ItemListContainer texto="Cuurly's Coffee Shop" />
+              </div>
+            }
+          />
+          <Route path="/detalle" element={<ItemDetailContainer />} />
+        </Routes>
+        <div className="App" id="fondo">
+          <Title greeting={texto} />
+          <Main />
         </div>
-        <ItemDetailContainer />
-      </div>
+      </BrowserRouter>
     </>
   );
 }
